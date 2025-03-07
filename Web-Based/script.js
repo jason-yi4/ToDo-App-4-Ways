1859a9a34701384ae3be992fbbb7d7992b36fbdb
jason-yi4 <jasonyi409@gmail.com>
2025-03-06 19:12:15 -0500

    form successfully adds tasks to a random list off to the right side. looks horrible, but it's progress

--- Web-Based/script.js
+++ Web-Based/script.js
@@ -1,57 +1,46 @@
 // submission form from HTML
-const form = document.getElementById("form");
+const form = document.getElementById("taskForm");
 
-// gets the new task input into the form, as a string
-let task = document.getElementById("task").value;
+let taskList = document.getElementById('taskList');
 
-
-
-/* setting tasks to an empty array if there is no existing
- * to-do list data.
- */
-
-// getting data from localStorage
-let tasks = localStorage.getItem("taskList");
-
-// checks if the localStorage element exists
-if (tasks) {
-
-    tasks = JSON.parse(tasks);
-
-    if (!Array.isArray(tasks)) {
-
-        // forces array instantiation for an invalid value
-        tasks = [];
-
-        console.log("localStorage JSON was not an Array. List of tasks reset to empty.");
-    }
-} else {
-    tasks = [];
-}
+// creates an array of tasks based on the localStorage JSON.
+let tasks = JSON.parse(localStorage.getItem('taskList')) || [];
 
 
 
-/* creating dynamic HTML element to constantly
- * display the current working to-do list
- * based on the localStorage JSON.
- */
+// renders all tasks on screen
+window.addEventListener("DOMContentLoaded", function() {
+    render()
+});
 
 
+// event listener for form submissions
+form.addEventListener("submit", function(event) {
 
+    // prevents page load
+    event.preventDefault();
 
+    // getting value of task
+    let taskInput = document.getElementById("task");
+    let task = taskInput.value.trim();
 
-
-/* event listening for submission of form.
- * executes the storeTask() method which stores the newly
- * added task into the localStorage JSON.
- */
-
-form.addEventListener("submit", storeTask());
-
-
-function storeTask() {
-    tasks.push(task)
-    console.log(`"${task}" added to list of tasks.`);
-
+    // pushes task to array
+    tasks.push(task);
     localStorage.setItem("taskList", JSON.stringify(tasks));
+    taskInput.value = ""; // reset value of form submission box
+    render();
+});
+
+
+// function updates the state of the page
+function render() {
+    // clears current list of tasks
+    taskList.innerHTML = '';
+
+    // dynamically creates HTML elements for the taskList HTML
+    tasks.forEach(function(task) {
+        let listItem = document.createElement("li");
+        listItem.textContent = task;
+        taskList.appendChild(listItem);
+    });
 }
