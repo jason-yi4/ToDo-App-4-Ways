066fc233d4e8fd05f71706befdfe2d14d0aa95ea
jason-yi4 <jasonyi409@gmail.com>
2025-03-06 20:01:36 -0500

    stacked elements vertically, improved checkbox functionality

--- Web-Based/script.js
+++ Web-Based/script.js
@@ -24,23 +24,50 @@ form.addEventListener("submit", function(event) {
     let taskInput = document.getElementById("task");
     let task = taskInput.value.trim();
 
-    // pushes task to array
-    tasks.push(task);
-    localStorage.setItem("taskList", JSON.stringify(tasks));
-    taskInput.value = ""; // reset value of form submission box
+    // pushes task to array if the task is a non-empty string literal
+    if (task) {
+        tasks.push(task);
+        console.log(`"${task}" added to the list of tasks.`);
+        localStorage.setItem("taskList", JSON.stringify(tasks));
+        taskInput.value = ""; // reset value of form submission box
+    } else {
+        console.log("No value for task.");
+    }
+
+
     render();
 });
 
 
-// function updates the state of the page
+/* event listener for checkbox ticks on to-do list */
+
+
+
+
+/* function updates the state of the page */
 function render() {
     // clears current list of tasks
     taskList.innerHTML = '';
 
     // dynamically creates HTML elements for the taskList HTML
     tasks.forEach(function(task) {
+        
+        // creating a list item to append to the taskList
         let listItem = document.createElement("li");
-        listItem.textContent = task;
+
+        // creating the task containing a checkbox
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        // creating text node
+        let textNode = document.createTextNode(task);
+
+        // appending checkbox and text
+        listItem.appendChild(checkbox);
+        listItem.appendChild(textNode);
+
+        // appending to the taskList
         taskList.appendChild(listItem);
+
     });
 }
