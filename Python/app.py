321dfb4d64a5af412743d08ea5e2a6f1b6e1930e
Jason Yi <154646656+jason-yi4@users.noreply.github.com>
2025-03-10 00:47:51 -0400

    Created main loop that updates the state of the app when modifications are made to the list of tasks.

--- Python/app.py
+++ Python/app.py
@@ -1,18 +1,35 @@
+import os
+
 '''
     CLI-based To-Do List
 
     Created by Jason Yi
 '''
 
-
-# refreshes CLI with current information
-def render():
-    pass
-
-
-
 # list of tasks
 tasks = []
 
+# refreshes CLI with current information
+def render():
+    print('------To-Do-----')
+    for task in tasks:
+        print(f'[] {task}')
+    
 
 
+# app loop
+while True:
+
+    # clears terminal between iterations
+    os.system('cls')
+
+
+    userInput = input("What's next?\n[1] - Add a task\n[2] - Complete a task\n[3] - Remove a task\n[exit]\n>>> ")
+
+
+
+    # breaks out of the loop and terminates the application
+    if userInput.lower() == 'exit':
+        break
+
+    render()
